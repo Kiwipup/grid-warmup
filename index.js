853383bc1e562e389d4d3a86dd8a8565a3b9f78d
Cmeredith <tina.wheezy@gmail.com>
2018-11-19 11:17:33 -0500

    trying to fix deployment issue

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var express= require('express');
 var app= express();
 var port= process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/vue-switchable-grid'));
 app.listen(port, function(){});
